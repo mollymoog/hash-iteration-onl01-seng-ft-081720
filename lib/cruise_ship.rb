f655e38c13d10d5f87fa87fe1380c4d6af91cfd7
molly mk <mmkover@gmail.com>
2020-08-23 00:08:30 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,8 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passenger.each do |suite, name|
+    if suite == :suite_a && name.start_with? ("A")
+      winner = name
 end
