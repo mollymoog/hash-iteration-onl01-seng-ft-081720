706b54e41e8819b6770e3157d02a0474e281006d
molly mk <mmkover@gmail.com>
2020-08-23 00:19:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,7 +12,10 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if birthday
+    if age < 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+      "You are too old for this."
   end    
 end 
 
